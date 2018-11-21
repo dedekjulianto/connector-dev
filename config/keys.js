32547d20f5e652ab0e596d7acffbd20bbe00847e
dedekjulianto <dedekproject@gmail.com>
2018-11-21 13:00:28 +0700

    test

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://dedek:root12@ds245210.mlab.com:45210/devconnectoroke',
+  secretOrKey: 'secret'
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+const app = express();
+
+// Body Parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 5000;
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB Connected");
+}).catch((err) => {
+  console.log(err);
+});
+
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
